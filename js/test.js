fcbb62658a7872e2339a84c2be40262fb05886d8
kuma-9106 <sakuyanful@gmail.com>
2020-11-10 09:21:19 +0000

    localStrage練習

--- js/test.js
+++ js/test.js
@@ -4,8 +4,61 @@
     
     var vm = new Vue({
         el: '#app',
-        data: {
-            name: 'Sakuma'
+        data: { 
+            newItem: '',
+            todos: [{
+                title: 'task 1',
+                isDone: false
+            },
+            {
+                title: 'task 2',
+                isDone: false
+            },
+            {
+                title: 'task 3',
+                isDone: true
+            }]
+        },
+        watch: {
+            todos: {
+                handler: function(){
+                    localStorage.setItem('todos', JSON.stringify(this.todos));
+                },
+                deep: true
+            }
+        },
+        mounted: function(){
+            this.todos = JSON.parse(localStorage.getItem('todos')) || [];
+        },
+        methods: {
+            addItem: function(){
+                var item = {
+                    title: this.newItem,
+                    isDone: false
+                }
+                this.todos.push(item);
+                this.newItem = '';
+            },
+            deleteItem: function(index){
+                if(confirm('Are you sure?')){
+                this.todos.splice(index,1);
+                }
+                
+            },
+            perge: function(){
+                if(!confirm('delete finished?')){
+                  return;
+                }
+                this.todos = this.remaining;
+                
+            }
+        },
+        computed: {
+            remaining: function (){
+                return this.todos.filter(function(todo){
+                    return !todo.isDone
+            });
+            }
         }
     });
     
