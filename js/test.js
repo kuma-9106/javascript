75aeedb6b5980bff98e1b6b70cc3db83465acc64
kuma-9106 <sakuyanful@gmail.com>
2020-11-11 07:35:17 +0000

    component練習

--- js/test.js
+++ js/test.js
@@ -2,63 +2,25 @@
     
     'use strict';
     
+    var likeComponent = Vue.extend({
+       props: ['msg'],
+       data: function(){
+           return {
+               count: 0
+           }
+       },
+       template: '<button @click="countUp">{{ count }} {{ msg }}</button>' ,
+       methods: {
+           countUp: function(){
+               this.count++;
+           }
+       }
+    });
+    
     var vm = new Vue({
         el: '#app',
-        data: { 
-            newItem: '',
-            todos: [{
-                title: 'task 1',
-                isDone: false
-            },
-            {
-                title: 'task 2',
-                isDone: false
-            },
-            {
-                title: 'task 3',
-                isDone: true
-            }]
-        },
-        watch: {
-            todos: {
-                handler: function(){
-                    localStorage.setItem('todos', JSON.stringify(this.todos));
-                },
-                deep: true
-            }
-        },
-        mounted: function(){
-            this.todos = JSON.parse(localStorage.getItem('todos')) || [];
-        },
-        methods: {
-            addItem: function(){
-                var item = {
-                    title: this.newItem,
-                    isDone: false
-                }
-                this.todos.push(item);
-                this.newItem = '';
-            },
-            deleteItem: function(index){
-                if(confirm('Are you sure?')){
-                this.todos.splice(index,1);
-                }
-                
-            },
-            perge: function(){
-                if(!confirm('delete finished?')){
-                  return;
-                }
-                this.todos = this.remaining;
-                
-            }
-        },
-        computed: {
-            remaining: function (){
-                return this.todos.filter(function(todo){
-                    return !todo.isDone
-            });
-            }
+        components: {
+            'like-component' : likeComponent
         }
     });
     
