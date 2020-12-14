0fea9edb79f051cac0560a8393ba65bd8785a5fb
kuma-9106 <sakuyanful@gmail.com>
2020-12-14 06:22:14 +0000

    js練習

--- js/calendar.js
+++ js/calendar.js
@@ -138,5 +138,10 @@ document.addEventListener('DOMContentLoaded', function() {
     }
     
     let btn = document.querySelectorAll('.btn');
+    
+    btn.addEventListener('click', e => {
+      var target = e.target.data;
+      console(target);
+    });
 
   });
