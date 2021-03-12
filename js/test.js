9726ea963ae67efd7240beaacca1386433a98bd9
kuma-9106 <sakuyanful@gmail.com>
2021-03-12 01:34:18 +0000

    ファイルの読み込み変更

--- js/test.js
+++ js/test.js
@@ -1,44 +1,10 @@
-(function(){
+document.addEventListener('DOMContentLoaded', function() {
+
+    let btn = document.querySelectorAll('.btn');
     
-    'use strict';
-    
-    var likeComponent = Vue.extend({
-       props: ['msg'],
-       data: function(){
-           return {
-               count: 0
-           }
-       },
-       template: '<button @click="countUp">{{ count }} {{ msg }}</button>' ,
-       methods: {
-           countUp: function(){
-               this.count++;
-               this.$emit('increment');
-           }
-       }
+    btn.addEventListener('click', e => {
+      var target = e.target.data;
+      console(target);
     });
-    
-    var vm = new Vue({
-        el: '#app',
-        components: {
-            'like-component' : likeComponent
-        },
-        data: {
-            total: 0,
-            currentTab: 1
-        },
-        methods: {
-            incrementTotal: function(){
-                this.total++;
-            },
-            toggle: function(num){
-               this.currentTab = num 
-            }
-        }
-    });
-    
-    
-    
-    
-    
-})();
+
+  });
